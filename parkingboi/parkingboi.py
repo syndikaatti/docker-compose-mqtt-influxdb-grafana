4adc077f46e01960b900fd50d350e017f19928b3
Paavo Pärssinen <paavo.parssinen@aalto.fi>
2019-05-05 11:00:48 +0300

    remove threaded

--- parkingboi/parkingboi.py
+++ parkingboi/parkingboi.py
@@ -77,4 +77,4 @@ api.add_resource(FreeParkingLocations, '/locations/free')
 api.add_resource(MapParkingLocations, '/maplocations')
 
 if __name__ == '__main__':
-    app.run(Threaded=True, debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
