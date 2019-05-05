2e99998ef256de1d4dd856617d9769d6248f0471
Joonas Maanonen <joonas.maanonen@aalto.fi>
2019-05-05 09:28:53 +0300

    Add rest api

--- parkingboi/parkingboi.py
+++ parkingboi/parkingboi.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 from geojson import Point, Feature
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask_restful import Resource, Api
 import json
 import os
 
 app = Flask(__name__)
-#app.config.from_object(__name__)
-#app.config.from_envvar('APP_CONFIG_FILE', silent=True)
-#MAPBOX_ACCESS_KEY = app.config['MAPBOX_ACCESS_KEY']
+api = Api(app)
 MAPBOX_ACCESS_KEY = os.environ['MAPBOX_ACCESS_KEY']
+LOCATIONSPATH = "/tmp/json/parkinglocations.json"
 
 @app.route('/')
 def mapbox_js():
@@ -17,6 +17,13 @@ def mapbox_js():
         'mapbox_js.html',
         ACCESS_KEY=MAPBOX_ACCESS_KEY,
         parking_locations=parking_locations)
+
+
+class ParkingLocations(Resource):
+    def get(self):
+        with open(LOCATIONSPATH, "r") as json_data:
+            locations = json.load()
+        return locations
 
 def get_parking_locations():
     parking_locations = []
@@ -36,9 +43,8 @@ def get_parking_locations():
             parking_locations.append(feature)
     return parking_locations
 
-# Read data from the database and returns a dict?
-def read_parking_data():
-    pass
+# Add all the rest api paths
+api.add_resource(ParkingLocations, '/locations')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
